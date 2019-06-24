50deea5370f7e7cd96200dbd49acd6f3add470ea
liangyuan531 <yuanliang123@hotmail.com>
2019-06-24 13:55:06 +1000

    delete comma at end of router in routes file

--- server/routes/records.route.js
+++ server/routes/records.route.js
@@ -1,9 +1,9 @@
 const recordsController = require('../controller/Records.ctrl')
 
 module.exports = (router) => {
-    router.route('/records/admin').get(recordsController.getAllRecords),
-    router.route('/records/add').post(recordsController.addRecord),
-    router.route('/records/:id').put(recordsController.updateRecord),
-    router.route('/records/:id').delete(recordsController.deleteRecord),
+    router.route('/records/admin').get(recordsController.getAllRecords)
+    router.route('/records/add').post(recordsController.addRecord)
+    router.route('/records/:id').put(recordsController.updateRecord)
+    router.route('/records/:id').delete(recordsController.deleteRecord)
     router.route('/records/user/:id').get(recordsController.getUserRecords)
 }
